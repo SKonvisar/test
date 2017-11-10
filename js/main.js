58dd43c67ebd51ad4d317463fc917b4a908cf461
SKonvisar <s.v.konvisar@gmail.com>
2017-11-10 22:56:52 +0200

    Add fonts, and mobile menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-$(document).ready(function(){
-	$(window).scroll(function(){
+var button = document.querySelector(".hamburger");
+var menu = document.getElementById('mnu');
+
+button.addEventListener('click', function(){
+	button.classList.toggle('is-active');
+	menu.classList.toggle('open');
+});
+
+$(document).ready(function() {
+   	$(window).scroll(function(){
 		var $head = $('#header');
-		if($(window).scrollTop() > $head.offset().top()){
-			$head.addClass("headerFixed");
+		if($(window).scrollTop() > 100){
+			$head.addClass("fixed");
 		} else {
-			$head.remoteClass('headerFixed');
+			$head.removeClass('fixed');
 		}
-	})
-})
+	});
+
+   	$('.price_text button').on('mouseenter',function(){
+   		$('.price_top span').addClass('hovered');
+   	});
+   	$('.price_text button').on('mouseleave',function(){
+   		$('.price_top span').removeClass('hovered');
+   	});
+});
